22f93cd00465a79d9385bd17d5b0b581aa964c9d
lentilek <laura.robinska@wp.pl>
2023-03-25 21:53:50 +0100

    pickables and next level
    one script to load next level and a needed amount of pickables to go to next level

--- szesciany/Assets/scripts/LoadMap2.cs
+++ szesciany/Assets/scripts/LoadMap2.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class LoadMap2 : MonoBehaviour
 {
+    public int neededPickUps = 3;
+    public string toLoad = "level_2";
+
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("level_2");
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>(); // to check if the collission is with the player, get the player's inventory component from the object that has been collided with
+
+        if (playerInventory.NumberOfPickups >= neededPickUps)
+        {
+            SceneManager.LoadScene(toLoad);
+        }
+
     }
 
     private void OnTriggerStay(Collider other)
