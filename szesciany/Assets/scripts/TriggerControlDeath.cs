e43f9ced8ebf2ae09501810aae53ce8e8a411e25
lentilek <laura.robinska@wp.pl>
2023-03-29 17:03:17 +0200

    particle on spikes

--- szesciany/Assets/scripts/TriggerControlDeath.cs
+++ szesciany/Assets/scripts/TriggerControlDeath.cs
@@ -5,11 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class TriggerControlDeath : MonoBehaviour
 {
+    public GameObject deathParticle;
+    public ParticleSystem death;
+    public float timer = 0;
+    public float howLong = 3;
+    public bool isDead = false;
+
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player"))
+        {
+            isDead = true;
+            timer = 0;
+            other.gameObject.SetActive(false);
+ 
+            death.Play();
+        }
 
     }
-
+    private void Update()
+    {
+        if(isDead)
+        {
+            timer += Time.deltaTime;
+            if(timer >= howLong)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
     private void OnTriggerStay(Collider other)
     {
 
@@ -18,6 +42,6 @@ public class TriggerControlDeath : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         //SceneManager.LoadScene("SampleScene");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //build index in the build settings>>file
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //build index in the build settings>>file
     }
 }
