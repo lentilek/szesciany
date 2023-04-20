79517356afb0f9dcf42fa7fcd4b134bfe9b958e4
lentilek <laura.robinska@wp.pl>
2023-04-20 15:43:05 +0200

    movement, camera, spawn fix

--- szesciany/Assets/scripts/CameraControl.cs
+++ szesciany/Assets/scripts/CameraControl.cs
@@ -8,10 +8,10 @@ public class CameraControl : MonoBehaviour
     public float pLerp = .02f;
     public float rLerp = .01f;
 
-    void Update()
+    private void FixedUpdate()
     {
-        //transform.Rotate(1f * Time.deltaTime, 0, 0);
         transform.position = Vector3.Lerp(transform.position, camTarget.position, pLerp);
         transform.rotation = Quaternion.Lerp(transform.rotation, camTarget.rotation, rLerp);
+
     }
 }
--- szesciany/Assets/scripts/CubeMovement.cs
+++ szesciany/Assets/scripts/CubeMovement.cs
@@ -6,9 +6,11 @@ using UnityEngine.Rendering;
 
 public class CubeMovement : MonoBehaviour
 {
-    public float speed = 5f; //public so it appears in unity and sets basic speed to 5
+    public float speed1 = 5f; //public so it appears in unity and sets basic speed to 5
+    public float speed2 = 0.5f; // speed in air
     public Rigidbody rb; //initialize Rigidbody component
     public bool cubeIsOnTheGround = true; //bool = returned as TRUE or FALSE; to check if floor is on the ground 
+    public float jumpForce = 10;
 
     void Start()
     {
@@ -18,19 +20,34 @@ public class CubeMovement : MonoBehaviour
     void Update()
     {
         //MOVING MECHANIC HERE//
-        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed; //time.deltaTime so it updates once per second
-        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-        transform.Translate(horizontal, 0, vertical); //can move on X and Z axis, not up and down on the Y
+       //transform.Translate(horizontal, 0, vertical); //can move on X and Z axis, not up and down on the Y
 
         // JUMPING MECHANIC HERE //
         //jump key = space in the input >> project manager >> InputManager 
         if (Input.GetButtonDown("Jump") && cubeIsOnTheGround) //&& = AND operator
         {
             SoundManagerScript.PlaySound("jumpsound"); // powinno wtedy wydawaæ dŸwiêk skoku -martyna
-            rb.AddForce(new Vector3(0, 7, 0), ForceMode.Impulse); //on each frame it adds this value of Y to the cube
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse); //on each frame it adds this value of Y to the cube
             cubeIsOnTheGround = false; //whenever we press SPACE cube ISNT on the ground
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (cubeIsOnTheGround) // change speed if in air
+        {
+            float horizontal = Input.GetAxis("Horizontal") * speed1; //time.deltaTime so it updates once per second
+            float vertical = Input.GetAxis("Vertical") * speed1;
+            rb.AddForce( new Vector3(horizontal, 0, vertical), ForceMode.VelocityChange); 
+        }
+        else
+        {
+            float horizontal = Input.GetAxis("Horizontal") * speed2; //time.deltaTime so it updates once per second
+            float vertical = Input.GetAxis("Vertical") * speed2;
+            rb.AddForce(new Vector3(horizontal, 0, vertical), ForceMode.VelocityChange);
+        }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
