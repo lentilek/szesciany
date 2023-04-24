d4ae7b91c2fa33d6ea55ac14a78a9b0fcedfc334
lentilek <laura.robinska@wp.pl>
2023-04-24 15:56:34 +0200

    camera distance + rotation limits

--- szesciany/Assets/scripts/CameraRotation.cs
+++ szesciany/Assets/scripts/CameraRotation.cs
@@ -6,11 +6,19 @@ public class CameraRotation : MonoBehaviour
 {
     public Vector2 turn;
     public float sensitivity = 2f;
+    public float lookUpMax = 20;   
+    public float lookUpMin = -60;
+    public float lookLeft = -90;
+    public float lookRight = 90;
 
     void Update()
     {
         turn.y += Input.GetAxis("Mouse Y") * sensitivity;
         turn.x += Input.GetAxis("Mouse X") * sensitivity;
+
+        turn.y = Mathf.Clamp(turn.y, lookUpMin, lookUpMax);
+        turn.x = Mathf.Clamp(turn.x, lookLeft, lookRight);
+
         transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
     }
 }
