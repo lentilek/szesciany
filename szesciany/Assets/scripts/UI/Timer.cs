8b0fb7e621f0a059f3924cf8ebc61cd9d5cf8ba2
lentilek <laura.robinska@wp.pl>
2023-05-18 09:58:44 +0200

    Level reload when times up

--- szesciany/Assets/scripts/UI/Timer.cs
+++ szesciany/Assets/scripts/UI/Timer.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public float time = 180f;
     public TextMeshProUGUI timerTxt;
+    public string toLoad;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +24,6 @@ public class Timer : MonoBehaviour
             yield return new WaitForSeconds(1f);
             time--;
         } while (time >= 0);
+        SceneManager.LoadScene(toLoad);
     }
 }
