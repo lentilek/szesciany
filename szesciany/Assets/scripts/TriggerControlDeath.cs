6c37a4f23fd7d478b07ba39b212b18e7e644a183
lentilek <laura.robinska@wp.pl>
2023-05-25 20:38:29 +0200

    lvl 1-5 new elements, working scale

--- szesciany/Assets/scripts/TriggerControlDeath.cs
+++ szesciany/Assets/scripts/TriggerControlDeath.cs
@@ -7,10 +7,15 @@ public class TriggerControlDeath : MonoBehaviour
 {
     public ParticleSystem death;
     public float howLong = 3;
-    public Skale scale;
+    private Skale scale;
 
     [SerializeField] GameObject player;
     [SerializeField] Transform spawnPoint;
+    private void Start()
+    {
+        scale = player.GetComponent<Skale>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
