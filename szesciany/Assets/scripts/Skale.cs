1824217b9b656d87b6b4a32c2e4ad3e9ed1200b9
lentilek <laura.robinska@wp.pl>
2023-06-01 19:39:42 +0200

    last changes. music, controls

--- szesciany/Assets/scripts/Skale.cs
+++ szesciany/Assets/scripts/Skale.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Skale : MonoBehaviour
 {
-    private float timeSmall = 2f;
+    private float timeSmall = 4f;
     private float betweenTime = 7f;
     public bool canBeSmall;
 
@@ -15,7 +15,7 @@ public class Skale : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z) && canBeSmall)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canBeSmall)
         {
             transform.localScale = new Vector3(0.5f,0.5f,0.5f);
             StartCoroutine(Small());
